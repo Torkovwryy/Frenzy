95d129830e53e1a251ac542c4936a87bc3dc4a7b
krebinkkj-tk <krebinkkj3@gmail.com>
2026-05-21 06:46:49 -0300

    chore: setup typescript, eslint, and prettier

---
+++ eslint.config.cjs
@@ -0,0 +1,54 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslintEslintPlugin = require("@typescript-eslint/eslint-plugin");
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+            tsconfigRootDir: __dirname,
+        },
+
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslintEslintPlugin,
+    },
+
+    extends: compat.extends(
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
+    ),
+
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/explicit-module-boundary-types": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-floating-promises": "error",
+    },
+}, globalIgnores(["**/eslint.config.cjs", "**/dist", "**/node_modules"])]);
